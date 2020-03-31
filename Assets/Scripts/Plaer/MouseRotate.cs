83f2f9621234c9027c1a6e28ccb008173de39a85
Егор Сидоров <egor25sidorov@gmail.com>
2020-03-31 17:33:12 +0300

    Камера от третьего лица

---
+++ Assets/Scripts/Plaer/MouseRotate.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseRotate : MonoBehaviour
+{
+    public enum RotationAxes
+    {
+        MouseXAndY = 0,
+        MouseX = 1,
+        MouseY = 2
+    }
+
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityHor = 9.0f;
+    public float sensitivityVert = 9.0f;
+    public float minimumVert = -45.0f;
+    public float maximumVert = 45.0f;
+    private float _rotationX = 0;
+
+    void Start()
+    {
+        Rigidbody body = GetComponent<Rigidbody>();
+        if (body != null)
+            body.freezeRotation = true;
+    }
+
+    void Update()
+    {
+        if(Input.GetAxis("Mouse X")!=0)
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHor, 0);
+    }
+}
--- Assets/Scripts/Plaer/PlaerController.cs
+++ Assets/Scripts/Plaer/PlaerController.cs
@@ -4,31 +4,29 @@ using UnityEngine;
 
 public class PlaerController : MonoBehaviour
 {
-    [SerializeField] private float speed;
-    [SerializeField] private float jumpForce;
+
+    public GameObject MainCamera;
+
+    [SerializeField] private float speed = 1.0f;
+    [SerializeField] private float jumpForce = 1.0f;
+
+
     private float moveHorizontal;
     private float moveVertical;
     private Rigidbody rb;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
 
     }
-
-    // Update is called once per frame
     void Update()
     {
-        //moveHorizontal = Input.GetAxis("Horizontal");
-        //moveVertical = Input.GetAxis("Vertical");
-        //Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        //rb.AddForce(movement * speed);
-
 
         if (Input.GetButton("Horizontal")) MoveX();
         if (Input.GetButton("Vertical")) MoveY();
         if (Input.GetButtonDown("Jump")) Jump();
+        
     }
 
     private void MoveX()
